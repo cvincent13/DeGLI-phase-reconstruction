7476cbae1b08e72b09052a276b26ec21958c1ba2
cvincent13 <13vincentcedric@gmail.com>
2024-07-07 14:49:06 +0200

    DNN model

--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
-
+import torch
+import torch.nn as nn
+import torch.functional as F
 
 #DNN
 class AIGCLayer(nn.Module):
@@ -14,9 +16,9 @@ class AIGCLayer(nn.Module):
         h_im = self.conv_re(x_im) + self.conv_im(x_re)
         h_gate = self.conv_gate(torch.cat([torch.abs(torch.complex(x_re, x_im)), amplitude], dim=1))
 
-        f_re = h_re * F.sigmoid(h_gate)
-        f_im = h_im * F.sigmoid(h_gate)
-        return f_re, f_im, F.sigmoid(h_gate)
+        f_re = h_re * h_gate.sigmoid()
+        f_im = h_im * h_gate.sigmoid()
+        return f_re, f_im, h_gate.sigmoid()
 
 
 
@@ -33,7 +35,7 @@ class AIGCNN(nn.Module):
         self.conv_re = nn.Conv2d(num_ch, 1, kernel_size=(1,1), stride=stride, bias=False, padding='same')
         self.conv_im = nn.Conv2d(num_ch, 1, kernel_size=(1,1), stride=stride, bias=False, padding='same')
                         
-    def forward(self, x_re, x_im, amp):
+    def forward(self, x_re, x_im, amp, return_gates=False):
         ampg = amp.unsqueeze(1)
 
         f_re, f_im, gate1 = self.layer1(x_re, x_im, ampg)
@@ -42,5 +44,8 @@ class AIGCNN(nn.Module):
         
         out_re = self.conv_re(f_re) - self.conv_im(f_im)
         out_im = self.conv_re(f_im) + self.conv_im(f_re)
-        out = torch.complex(out_re, out_im).squeeze()        
-        return out, (gate1, gate2, gate3)
+        out = torch.complex(out_re, out_im).squeeze()
+        if return_gates:
+            return out, (gate1, gate2, gate3)
+        else:
+            return out
